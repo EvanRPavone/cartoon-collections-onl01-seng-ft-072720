7e9235e5a79278beeb04792a6f9b4096a97249d1
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 19:11:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  # Your code here
+  array.any? do |word|
+    word.length > 4
 end
 
 def find_the_cheese(array)# code an argument here
